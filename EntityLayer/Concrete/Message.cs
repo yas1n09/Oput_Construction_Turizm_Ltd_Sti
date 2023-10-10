c2345cdb797a2ffd9eae11777175fade9f211a27
yasinyagci0990@gmail.com <yasinyagci0990@gmail.com>
2023-10-10 19:11:27 +0300

    Proje dosyası ekle.

---
+++ EntityLayer/Concrete/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Message
+    {
+        public int MessageID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string ConctactNumber { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ Oput_Construction_Turizm_Ltd_Sti/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Oput_Construction_Turizm_Ltd_Sti.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
