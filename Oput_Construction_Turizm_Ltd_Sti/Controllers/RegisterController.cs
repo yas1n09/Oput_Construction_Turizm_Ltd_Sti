429b387c038f638616daa9d3187be7a16801c8b2
yasinyagci0990@gmail.com <yasinyagci0990@gmail.com>
2023-10-22 19:23:55 +0300

    -

---
+++ Oput_Construction_Turizm_Ltd_Sti/Controllers/RegisterController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Oput.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string p)
+        {
+            return View();
+        }
+    }
+}
