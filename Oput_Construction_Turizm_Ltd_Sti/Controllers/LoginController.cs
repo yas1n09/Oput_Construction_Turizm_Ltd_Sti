cd4ea37de5fdeea5d6928517b1ef4423d6a62a61
yasinyagci0990@gmail.com <yasinyagci0990@gmail.com>
2023-10-23 14:03:53 +0300

    Login Done

---
+++ Oput_Construction_Turizm_Ltd_Sti/Controllers/LoginController.cs
@@ -0,0 +1,52 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Oput.Models;
+
+namespace Oput.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        private readonly SignInManager<WriterUser> _signInManager;
+
+        public LoginController(SignInManager<WriterUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserLoginViewModel p)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(p.Email, p.Password, false, true);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Feature");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Hatalı kullanıcı adı veya şifre");
+                }
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Login");
+        }
+
+    }
+}
---
+++ Oput_Construction_Turizm_Ltd_Sti/Models/UserLoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Oput.Models
+{
+    public class UserLoginViewModel
+    {
+        [Display(Name = "Kullanıcı Adı")]
+        [Required(ErrorMessage = "Kullanıcı Adını giriniz...!")]
+        public string Email { get; set; }
+
+
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage = "Şifreyi giriniz...!")]
+        public string Password { get; set; }
+    }
+}
